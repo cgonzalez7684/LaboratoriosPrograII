97abadf0325c58d6756a8fa80599001638000744
DavidCorella <123708119+DavidCorella@users.noreply.github.com>
2023-02-16 21:08:54 -0600

    Carga 2

--- Laboratorio#2/DavidCorellaSanchez/GestionVentas.py
+++ Laboratorio#2/DavidCorellaSanchez/GestionVentas.py
@@ -58,6 +58,7 @@ def imprimirfacturas():
         print("El monto de la factura es ",n.montofactura) 
         #El monto de la factura es 458789
         print("El descuento de la factura es ",n.descuento*100) 
-        print("El descuento de la factura es ",n.fechafactura) 
+        print("El descuento de la factura es ",n.fechafactura)
+        print("El descuento de la factura es ",n.fechafactura)  
         
 
