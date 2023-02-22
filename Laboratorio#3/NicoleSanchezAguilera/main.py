d8e068676bc0e9994aa2e5067ffb65838862052e
nicolesanch5 <124539549+nicolesanch5@users.noreply.github.com>
2023-02-22 07:16:37 -0600

    actualizar

---
+++ Laboratorio#3/NicoleSanchezAguilera/main.py
@@ -0,0 +1,4 @@
+#Elabore un programa tipo consola que permita ejemplificar las acciones de agregar, eliminar, 
+#modificar y recorrer una estructura tipo <list>, el contexto del desarrollo queda a criterio del estudiante.
+
+#Funciones auxiliares 
