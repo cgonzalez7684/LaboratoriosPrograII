532093126e1568250172af2d8d31d34b6a6cc1de
theMax204 <123708158+theMax204@users.noreply.github.com>
2023-02-23 01:30:21 -0700

    prueba

--- Laboratorio#2/MaximilianooCetrolaMIjalchuk/Dominio.py
+++ Laboratorio#2/MaximilianooCetrolaMIjalchuk/Dominio.py
@@ -6,23 +6,9 @@ class Factura:
         self.impuestofactura = None 
         self.nombreCliente = None
         self.categoriaVenta = None
-        self.montomasdescuento = 0 #Lugar donde se almacena el monto con el descuento
-        self.descuentoopcional = 0 #variable para el despuesto opcional
-
+    
     def calculaImpuesto(self):
         self.impuestofactura = self.montofactura * 0.13
         return self.impuestofactura
     
-    #Metodo nuevo para el calculo del descuento
-    def calculodeldescuento(self, categoria):
-        if (categoria == "A")
-         self.montomasdescuento = self.montofactura - (self.montofactura * 0.05)
-        elif (categoria == "B")
-         self.montomasdescuento = self.montofactura - (self.montofactura * 0.10)
-        else:
-           self.descuentoopcional - input("Digite el porcentaje del descuento;\n")
-           self.montomasdescuento - self.montofactura - (self.montofactura * self.descuentoopcional)
-
-        return self.descuentoopcional
-        
 listadoFacturas = []
