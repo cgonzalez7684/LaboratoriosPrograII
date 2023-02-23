a8898f431598f18239692584270d670e45255be9
Jason Brenes <123708108+jbrenes1984@users.noreply.github.com>
2023-02-22 22:55:40 -0600

    Lab3JasonBrenesSalgado

---
+++ Laboratorio#3/JasonBrenesSalgado/main.py
@@ -0,0 +1,27 @@
+import GestionListado as gl
+
+
+def main():
+    1
+    while True:
+        gl.encabezadoMenu()
+        
+        opcion = int(input("Digite una opción del sistema: "))
+        if(opcion == 1):
+            nombrefruta = input("Nombre de la fruta que desea agregar : ")
+            gl.crearListado(nombrefruta)
+        elif (opcion == 2 ):
+            nombrefruta = input("Nombre la fruta que desea eliminar : ")
+            gl.eliminarFruta(nombrefruta)
+        elif (opcion == 3 ):
+            nombrefruta = input("Nombre de la fruta que desea modificar : ")
+            nuevafruta = input ("Agregue el nombre de la nueva fruta : ")
+            gl.modificarFruta(nombrefruta,nuevafruta)
+        elif (opcion == 4):
+            gl.imprimirLista()
+        else:
+            continue
+
+
+if __name__=="__main__":
+    main()
