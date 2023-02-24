e659b4da5a03ca547ae2d7cccb2fde78de30103f
Kevin Salgado <kev.salgado@icloud.com>
2023-02-23 20:22:33 -0600

    KevinSalgado_Lab3.v2

---
+++ Laboratorio#3/KevinSalgadoCalderón/Listas_KevinSalgado.py
@@ -0,0 +1,19 @@
+# Una lista con las edades de los alumnos de la clase
+
+edades = [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+print(edades) #Imprimir la lista
+
+print(edades[1]) #Acceder a un elemento de la lista
+
+edades.append(31) #Añadir un elemento a la lista
+print("Despues del append: ", edades)
+
+edades[18] = 17 #Cambiar un elemento de la lista
+
+del edades[27] #Eliminar un elemento de la lista
+
+for edad in edades: #Recorrer la lista
+    print(edad)
+
+
+
